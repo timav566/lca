f59cca5ee178cfc06359f6c16c6d699781aab8fe
Maria Tigina <maria.tigina@jetbrains.com>
2023-07-24 18:21:21 +0200

    Add batch processing

--- lca/data_collection/collect/repo_processor.py
+++ lca/data_collection/collect/repo_processor.py
@@ -7,6 +7,10 @@ class RepoProcessor(abc.ABC):
     def __init__(self, github_tokens: list[str]):
         self.github_tokens = github_tokens
 
+    async def process_repositories_in_batches(self, repositories: list[tuple], batch_size: int = 10):
+        for i in range(0, len(repositories), batch_size):
+            await self.process_repositories(repositories[i:i + batch_size])
+
     async def process_repositories(self, repositories: list[tuple]):
         prepare_repositories_coroutines = []
         for i, (owner, name) in enumerate(repositories):
